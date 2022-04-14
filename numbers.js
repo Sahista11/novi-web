3c8d3e7b1981ec5aa9fa5d1e77ad8d13e2a44e39
Sahista11 <bruno.pavcevic5@gmail.com>
2022-04-14 23:45:24 +0200

    a
    a

--- numbers.js
+++ numbers.js
@@ -1,50 +1,51 @@
 const counters = document.querySelectorAll(".counter");
 const membersSection = document.querySelectorAll(".members-section");
+var notDone = true;
 
 document.addEventListener("scroll", function () {
   const clientHeight = document.documentElement.clientHeight;
-  const membersSectionY = membersSection.getBoundingClientRect().y;
+  const membersSectionY = membersSection[0].getBoundingClientRect().y;
 
-  if (clientHeight > membersSectionY) {
+  if (clientHeight > membersSectionY && notDone) {
+    counters.forEach((counter) => {
+      counter.innerText = "0";
+
+      const updateCounter = () => {
+        const target = +counter.getAttribute("data-target");
+        const c = +counter.innerText;
+
+        const increment = target / 200;
+
+        if (c < target) {
+          counter.innerText = `${Math.ceil(c + increment)}`;
+          setTimeout(updateCounter, 30);
+        } else {
+          counter.innerText = target + "+";
+        }
+      };
+      updateCounter();
+    });
+
+    const counterBign = document.querySelectorAll(".counter-bn");
+
+    counterBign.forEach((counterBn) => {
+      counterBn.innerText = "0";
+
+      const updateCounterBn = () => {
+        const targetBn = +counterBn.getAttribute("data-target");
+        const b = +counterBn.innerText;
+
+        const incrementBn = targetBn / 100;
+
+        if (b < targetBn) {
+          counterBn.innerText = `${Math.ceil(b + incrementBn)}`;
+          setTimeout(updateCounterBn, 8);
+        } else {
+          counterBn.innerText = targetBn + "+";
+        }
+      };
+      updateCounterBn();
+    });
+    notDone = false;
   }
-});
-
-counters.forEach((counter) => {
-  counter.innerText = "0";
-
-  const updateCounter = () => {
-    const target = +counter.getAttribute("data-target");
-    const c = +counter.innerText;
-
-    const increment = target / 200;
-
-    if (c < target) {
-      counter.innerText = `${Math.ceil(c + increment)}`;
-      setTimeout(updateCounter, 30);
-    } else {
-      counter.innerText = target + "+";
-    }
-  };
-  updateCounter();
-});
-
-const counterBign = document.querySelectorAll(".counter-bn");
-
-counterBign.forEach((counterBn) => {
-  counterBn.innerText = "0";
-
-  const updateCounterBn = () => {
-    const targetBn = +counterBn.getAttribute("data-target");
-    const b = +counterBn.innerText;
-
-    const incrementBn = targetBn / 100;
-
-    if (b < targetBn) {
-      counterBn.innerText = `${Math.ceil(b + incrementBn)}`;
-      setTimeout(updateCounterBn, 10);
-    } else {
-      counterBn.innerText = targetBn + "+";
-    }
-  };
-  updateCounterBn();
 });
