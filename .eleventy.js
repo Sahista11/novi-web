6fdafda7a66427a2a8fcf9b5bd6a34b133309217
Sahista11 <bruno.pavcevic5@gmail.com>
2022-06-09 14:44:53 +0200

    fixed admin files

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/nav.js");
   eleventyConfig.addPassthroughCopy("./src/numbers.js");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
